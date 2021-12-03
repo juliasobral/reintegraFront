1ce220aeea70ecf8f430ba9bd6f877fc3f9dd9d4
juliasobral <juliasobralm1@gmail.com>
2021-12-03 09:58:35 -0300

    @juliasobral [criação de componentes .menu .rodape.]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { PostagensComponent } from './postagens/postagens.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent,
+    CadastroComponent,
+    PostagensComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
