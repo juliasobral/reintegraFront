a4c4de2bdbe3d3b2457d5ec37f263ac46bc11445
unknown <cecilia.soares13@gmail.com>
2021-12-15 12:46:35 -0300

    @cecicsoares [PUT e DELETE de tema]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastro/cadastro.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { PostagensComponent } from './postagens/postagens.component';
@@ -9,29 +11,16 @@ import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+{path: '', redirectTo: 'login', pathMatch: 'full'},
 {path: 'login', component: LoginComponent},
 {path: 'cadastro', component: CadastrarComponent},
 {path: 'postagens', component: PostagensComponent},
 {path: 'sobrenos', component: SobreNosComponent},
 {path: 'tema', component: TemaComponent},
 
-// {path: 'tema-edit/:id', _component: TemaEditComponent,
-// get component() {
-//     return this._component;
-//   },
-// set component(value) {
-//     this._component = value;
-//   },
-// },
-// {path: 'tema-delete/id', _component_1: TemaDeleteComponent,
-// get component() {
-//     return this._component_1;
-//   },
-// set component(value) {
-//     this._component_1 = value;
-//   },
-// }
+{path: 'tema-edit/:id', component: TemaEditComponent},
+
+{path: 'tema-delete/id', component: TemaDeleteComponent}
 ];
 
 @NgModule({
